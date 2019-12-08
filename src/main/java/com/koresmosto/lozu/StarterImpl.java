b45561435edbe2dffa0a875dd4d6102b75142199
koresmosto <koresmosto@gmail.com>
2019-12-08 18:10:00 +0200

    Fix formatting

--- src/main/java/com/koresmosto/lozu/StarterImpl.java
+++ src/main/java/com/koresmosto/lozu/StarterImpl.java
@@ -4,6 +4,18 @@ public class StarterImpl implements Starter {
 
   private Randomer randomer = new RandomerImpl();
 
+  /**
+   * Standard java main method.
+   *
+   * @param args standard main method args
+   */
+  public static void main(String[] args) {
+    Starter starter = new StarterImpl();
+    if (args != null) {
+      System.out.println("Welcome " + starter.get());
+    }
+  }
+
   public String get() {
     return "Starter";
   }
@@ -15,17 +27,5 @@ public class StarterImpl implements Starter {
   public Object getRandom(String randomerType) {
 
     return randomer.getRandom(randomerType);
-  }
-
-  /**
-   * Standard java main method.
-   *
-   * @param args standard main method args
-   */
-  public static void main(String[] args) {
-    Starter starter = new StarterImpl();
-    if (args != null) {
-      System.out.println("Welcome " + starter.get());
-    }
   }
 }
